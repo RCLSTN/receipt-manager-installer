17ea40374d2ed140cb74b4bc849729d9801f14bc
Rutherford County Library System <dev@rclstn.org>
2024-04-04 16:25:04 -0500

    Update external_installer.bat
    package versions set

--- external_installer.bat
+++ external_installer.bat
@@ -1,7 +1,8 @@
 @echo off
 :: Use pip to install "python-escpos"
 echo Installing python-escpos...
-C:/Python39/Scripts/pip.exe install python-escpos
+C:/Python39/Scripts/pip.exe install python-escpos==2.2.0
+C:/Python39/Scripts/pip.exe install pillow==9.5.0
 C:/Python39/Scripts/pip.exe install requests
 C:/Python39/Scripts/pip.exe install psutil
 
